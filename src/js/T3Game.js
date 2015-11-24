f32e0f75ab7d9a7fed44e7d430dd880b3989a225
sergejgasjuk <sergejgasjuk@gmail.com>
2015-11-25 00:34:48 +0200

    edits

--- src/js/T3Game.js
+++ src/js/T3Game.js
@@ -1,27 +1,31 @@
 import React from "react";
-import ReactDom from "react-dom";
 import classNames from "classnames/bind";
+
+let css = {
+  game      : "t3-game",
+  panel     : "t3-panel",
+  panelX    : "t3-panel--active-x",
+  panelO    : "t3-panel--active-o",
+  panelItem : "t3-panel-item",
+  field     : "t3-field",
+  fieldX    : "t3-field--active-x",
+  fieldO    : "t3-field--active-o",
+  row       : "t3-row",
+  cell      : "t3-cell",
+  cellX     : "t3-cell--x",
+  cellO     : "t3-cell--o",
+  cellXWin  : "t3-cell--x_winner",
+  cellOWin  : "t3-cell--o_winner",
+  potentialX: "t3-cell--potential-x",
+  potentialO: "t3-cell--potential-o",
+  text      : "t3-text",
+  textBold  : "t3-text--bold",
+  textSmall : "t3-text--small"
+};
+let cx = classNames.bind(css);
 
 const CELL_X = 1;
 const CELL_O = -1;
-
-let css = {
-  game  : "t3-game",
-  panel : "t3-panel",
-  panelX: "t3-panel--active-x",
-  panelO: "t3-panel--active-o",
-  field : "t3-field",
-  row   : "t3-row",
-  cell  : "t3-cell",
-  cellOccupied: "t3-cell--occupied",
-  cellX : "t3-cell--x",
-  cellO : "t3-cell--o",
-  cellXWin: "t3-cell--x_winner",
-  cellOWin: "t3-cell--o_winner",
-  cellXHover: "t3-cell--hover-x",
-  cellOHover: "t3-cell--hover-o"
-};
-let cx = classNames.bind(css);
 
 let T3Game = React.createClass({
   getInitialState: function() {
@@ -31,21 +35,10 @@ let T3Game = React.createClass({
         [0, 0, 0],
         [0, 0, 0]
       ],
-      winCombos: [
-        [{y: 0, x: 0}, {y: 0, x: 1}, {y: 0, x: 2}],
-        [{y: 0, x: 0}, {y: 1, x: 0}, {y: 2, x: 0}],
-        [{y: 1, x: 0}, {y: 1, x: 1}, {y: 1, x: 2}],
-        [{y: 0, x: 1}, {y: 1, x: 1}, {y: 2, x: 1}],
-        [{y: 2, x: 0}, {y: 2, x: 1}, {y: 2, x: 2}],
-        [{y: 0, x: 2}, {y: 1, x: 2}, {y: 2, x: 2}],
-        [{y: 0, x: 0}, {y: 1, x: 1}, {y: 2, x: 2}],
-        [{y: 0, x: 2}, {y: 1, x: 1}, {y: 2, x: 0}]
-      ],
-      activeSign: CELL_X,
+      activeSign: this.setRandomSign(),
       isActiveGame: true,
       isDrawGame: false,
-      isWinGame: false,
-      winSign: null
+      isWinGame: false
     }
   },
 
@@ -77,10 +70,9 @@ let T3Game = React.createClass({
     if (win) {
       console.log("is win");
       let winRow = win.row;
-      let winSign = win.sign;
       isWinGame = !isWinGame;
 
-      this.setState({isWinGame, winRow, winSign});
+      this.setState({isWinGame, winRow});
     } else if (draw) {
       console.log("is draw");
       isDrawGame = !isDrawGame;
@@ -91,7 +83,7 @@ let T3Game = React.createClass({
     this.setState({isActiveGame});
   },
 
-  checkWin() {
+  checkWin: function() {
     let {field} = this.state;
     let winCombos = [
       [{y: 0, x: 0}, {y: 0, x: 1}, {y: 0, x: 2}],
@@ -119,17 +111,21 @@ let T3Game = React.createClass({
       n += 1;
     }
 
-    return sign && row ? {sign, row} : false;
+    return row ? {row} : false;
   },
 
-  checkEmptyCells() {
+  setRandomSign: function() {
+    return Math.floor(Math.random() * 2) === CELL_X ? CELL_X : CELL_O;
+  },
+
+  checkEmptyCells: function() {
     let {field} = this.state;
 
     return field[0].some((el) => el === 0) || field[1].some((el) => el === 0) || field[2].some((el) => el === 0);
   },
 
   resetGame: function() {
-    let {field, activeSign, isActiveGame, isDrawGame, isWinGame, winRow, winSign} = this.state;
+    let {field, activeSign, isActiveGame, isDrawGame, isWinGame, winRow} = this.state;
 
     field.forEach((row, y) => {
       row.forEach((cell, x) => {
@@ -137,20 +133,26 @@ let T3Game = React.createClass({
       });
     });
 
-    activeSign = CELL_X;
+    activeSign = this.setRandomSign();
     isActiveGame = true;
     isWinGame = isDrawGame = false;
-    winRow = winSign = null;
 
-    this.setState({field, activeSign, isActiveGame, isDrawGame, isWinGame, winRow, winSign});
+    this.setState({field, activeSign, isActiveGame, isDrawGame, isWinGame, winRow});
   },
 
   render: function() {
-    let {field, isActiveGame, isWinGame, isDrawGame, winSign, activeSign} = this.state;
+    let {field, isActiveGame, isWinGame, isDrawGame, activeSign} = this.state;
+    let fieldClassName = cx(
+      'field',
+      {
+        fieldX: activeSign === CELL_X && isActiveGame,
+        fieldO: activeSign === CELL_O && isActiveGame
+      }
+    );
 
     return (
       <div className={cx('game')}>
-        <div className={cx('field')}>
+        <div className={fieldClassName}>
           {field.map((row, y) =>
             <div className={cx('row')} key={`r_${y}`}>
               {row.map((cell, x) =>
@@ -160,6 +162,8 @@ let T3Game = React.createClass({
                   posY={y}
                   posX={x}
                   isActiveGame={isActiveGame}
+                  activeSign={activeSign}
+                  isOccupied={field[y][x] !== 0}
                   key={`c_y=${y}_x=${x}`}/>
               )}
             </div>
@@ -170,7 +174,6 @@ let T3Game = React.createClass({
           isActiveGame={isActiveGame}
           isWinGame={isWinGame}
           isDrawGame={isDrawGame}
-          winSign={winSign}
           activeSign={activeSign}/>
       </div>
     )
@@ -186,12 +189,16 @@ let T3Cell = React.createClass({
   },
 
   render: function() {
-    let cellVal = this.props.cellVal;
+    let {cellVal, isOccupied, activeSign, isActiveGame} = this.props;
+    let isPotential = isActiveGame && !isOccupied;
+
     let className = cx(
       'cell',
       {
         cellX: cellVal === CELL_X,
-        cellO: cellVal === CELL_O
+        cellO: cellVal === CELL_O,
+        potentialX: isPotential && activeSign === CELL_X,
+        potentialO: isPotential && activeSign === CELL_O
       }
     );
     return (
@@ -204,37 +211,45 @@ let T3Cell = React.createClass({
 
 let T3Panel = React.createClass({
   render: function() {
-    let {activeSign, winSign, isActiveGame, isWinGame, isDrawGame} = this.props;
+    let {activeSign, isActiveGame, isWinGame, isDrawGame} = this.props;
     let gameOn = isActiveGame && !(isWinGame || isDrawGame);
     let gameOff = !isActiveGame && (isWinGame || isDrawGame);
+    let winSign = activeSign === CELL_X ? CELL_O : CELL_X;
+    let activeSignConverted = activeSign === CELL_X ? "X" : "O";
+    let winSignConverted = winSign === CELL_X ? "X" : "O";
+    let panelClassName = cx(
+      'panel',
+      {
+        panelX: activeSign === CELL_X && isActiveGame,
+        panelO: activeSign === CELL_O && isActiveGame
+      }
+    );
 
-    let win = activeSign === CELL_X ? CELL_O : CELL_X;
-    activeSign = activeSign === CELL_X ? "X" : "O";
-    console.log("ac: " ,activeSign)
-    console.log("win: " ,win)
-    win = win === CELL_X ? "X" : "O";
     return (
       <div
-        className={cx(
-          'panel',
-          {
-            panelX: activeSign === CELL_X && isActiveGame,
-            panelO: activeSign === CELL_O && isActiveGame
-          }
-        )}
+        className={panelClassName}
         onClick={gameOff ? this.props.onReset : null}>
         {gameOn &&
-          <div>Player: <b>{activeSign}</b></div>
+          <div className={cx('panelItem')}>
+            <span className={cx('text')}> Player: </span>
+            <b className={cx('text', 'textBold')}> {activeSignConverted} </b>
+          </div>
         }
         {gameOff &&
           <div>
             {isDrawGame &&
-              <div>It's <b>Draw!</b></div>
+              <div className={cx('panelItem')}>
+                <span className={cx('text')}> It's </span>
+                <b className={cx('text', 'textBold')}> Draw! </b>
+              </div>
             }
             {isWinGame &&
-              <div>Winner: <b>{win}</b></div>
+              <div className={cx('panelItem')}>
+                <span className={cx('text')}> Winner: </span>
+                <b className={cx('text', 'textBold')}> {winSignConverted} </b>
+              </div>
             }
-            <small>Click to start again!</small>
+            <small className={cx('text', 'textSmall')}> Click to start again! </small>
           </div>
         }
       </div>
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 import T3Game from "./T3Game";
 
-ReactDom.render(<T3Game/>, document.getElementById("container"));
+ReactDOM.render(<T3Game/>, document.getElementById("container"));
